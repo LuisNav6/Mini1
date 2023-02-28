2e9dc42497a1e6d553e0e777a91e55c2c8172a5c
LuisNav6 <113213452+LuisNav6@users.noreply.github.com>
2023-02-28 15:55:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 // Variables globales
-let alias = "";
-let score = 0;
-let bestTime = 0;
-
+var alias = "";
+var score = 0;
+var bestTime = 0;
+var players = localStorage.getItem("players");
+ players = JSON.parse(players);
+ if(players == null) players = [];
 // Variables DOM
 document.getElementById("start-button").addEventListener("click", function () {
     getAlias();
 });
+document.getElementById("register-button").addEventListener("click", function () {
+  alta();
+});
 const aliasSpan = document.getElementById("alias");
-
+//para registrar usuarios
+function alta(){
+  let aliasInput = document.getElementById("name").value;
+  if (aliasInput.trim() === "") {
+    alert("El alias no puede estar vacío. Intenta de nuevo.");
+  }
+  var p = JSON.stringify({
+    alias : aliasInput,
+    time: 0,
+    score : 0,
+    bestTime: 0,
+  });
+  players.push(p);
+  localStorage.setItem("players",JSON.stringify(players));
+  alert("dado de alta")
+}
 // Función para obtener el alias del jugador
 function getAlias() {
 
@@ -20,15 +40,23 @@ function getAlias() {
     alert("El alias no puede estar vacío. Intenta de nuevo.");
   }
   // Si el alias ya existe en el localstorage, se obtienen sus datos
-  else if (localStorage.getItem("PlayerStats")) {
-    let playerData = JSON.parse(localStorage.getItem("PlayerStats"));
-    alias = aliasInput;
-    score = playerData.score;
-    bestTime = playerData.bestTime;
-    alert(
-      `Bienvenido de nuevo, ${alias}! Tu puntaje actual es ${score} y tu mejor tiempo es ${bestTime} segundos.`
-    );
-    startGame();
+  else if (localStorage.getItem("players")) {
+    var playerData = JSON.parse(localStorage.getItem("players"));
+    for(var i in playerData){
+      let p = JSON.parse(playerData[i]);
+      alias = p.alias;
+      score = p.score;
+      bestTime = p.bestTime;
+      if(p.alias == aliasInput){
+        alert(
+          `Bienvenido de nuevo, ${alias}! Tu puntaje actual es ${score} y tu mejor tiempo es ${bestTime} segundos.`
+        );
+        startGame();
+      }else{
+        alta();
+      }
+    }
+   
   }
   // Si el alias no existe en el localstorage, se crea un nuevo registro
   else {
@@ -36,7 +64,7 @@ function getAlias() {
     localStorage.setItem(
       "Player",
       JSON.stringify({
-        alias: alias,
+      alias: alias,
       })
     );
     alert(`Bienvenido, ${alias}! Empieza a jugar y diviértete.`);
